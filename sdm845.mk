919c8d15eced612015b9581105f3ce32f1aa72c5
Darren Ramen <darren.ramen@gmail.com>
2020-11-22 22:27:53 +0400

    sdm845-common: import drm blobs from instantnoodlep
    * OnePlus/OnePlus8Pro_IND/OnePlus8Pro:11/RP1A.201005.001/2009301800:user/release-keys

--- sdm845.mk
+++ sdm845.mk
@@ -198,12 +198,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     LGEDoze
 
-# DRM
-PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service \
-    android.hardware.drm@1.3-service.clearkey
-
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.lge_sdm845
