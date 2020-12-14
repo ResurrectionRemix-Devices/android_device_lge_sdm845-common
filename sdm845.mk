774fcc3d0280b9d6b8df902d2990bb3041e4c453
Emanuel <emanuelghub@gmail.com>
2020-12-14 14:46:55 +0400

    Revert "Bump QTI btconfigstore to 2.0"
    This reverts commit 84c858201d9cb7b29fdb71e603a22fe2a7a51753.

--- sdm845.mk
+++ sdm845.mk
@@ -149,7 +149,7 @@ PRODUCT_PACKAGES += \
     liba2dpoffload \
     libbthost_if \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
-    vendor.qti.hardware.btconfigstore@2.0.vendor
+    vendor.qti.hardware.btconfigstore@1.0.vendor
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 3120
