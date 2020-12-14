f5a67ba9264c20d36a3177e2ae6f41b25a34b94c
Emanuel <emanuelghub@gmail.com>
2020-12-14 14:46:57 +0400

    Build BluetoothQti

--- sdm845.mk
+++ sdm845.mk
@@ -148,6 +148,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     liba2dpoffload \
     libbthost_if \
+    BluetoothQti \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor
 
