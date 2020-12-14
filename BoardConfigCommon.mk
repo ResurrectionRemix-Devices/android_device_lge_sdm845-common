ba5555c8f3ca926801861ff616e2e1f2f0ac8c93
Emanuel <emanuelghub@gmail.com>
2020-12-14 14:46:56 +0400

    Revert "selinux: Evo bringup"
    This reverts commit 97c687d046a3a3e01194e34855bff1637b05307e.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -178,8 +178,8 @@ TARGET_RIL_VARIANT := caf
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
-BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
+#BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
+#BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
 # Treble
 BOARD_VNDK_VERSION := current
